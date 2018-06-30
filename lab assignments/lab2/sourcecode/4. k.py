91a9025ba3dcacf73b36626e22b32a78c9738560
kiranrao139 <35550763+kiranrao139@users.noreply.github.com>
2018-06-29 22:57:04 -0500

    Update 4. k.py

--- lab assignments/lab2/sourcecode/4. k.py
+++ lab assignments/lab2/sourcecode/4. k.py
@@ -1,6 +1,3 @@
-#4)Report your views on the k nearest neighbor algorithm when we change the K how it will affect the accuracy.
-# Provide a good justification about the changes of the accuracy when we change the amount of K.
-# For example: compare the accuracy when K=1 and K is a big number like 50, why the accuracy will change
 # importing all the sklearn packages
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
